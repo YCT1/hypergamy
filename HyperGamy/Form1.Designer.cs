5d671ca9418b2bea41f8ded9724798a19c54c314
YCT1 <yektacan1999@gmail.com>
2018-11-14 16:21:48 +0300

    Temel Testler

--- HyperGamy/Form1.Designer.cs
+++ HyperGamy/Form1.Designer.cs
@@ -28,12 +28,88 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Yarat = new System.Windows.Forms.Button();
+            this.listbox = new System.Windows.Forms.ListBox();
+            this.Group = new System.Windows.Forms.Button();
+            this.groupList = new System.Windows.Forms.ListBox();
+            this.showSMV = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Yarat
+            // 
+            this.Yarat.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Yarat.AutoSize = true;
+            this.Yarat.Location = new System.Drawing.Point(12, 460);
+            this.Yarat.Name = "Yarat";
+            this.Yarat.Size = new System.Drawing.Size(266, 45);
+            this.Yarat.TabIndex = 0;
+            this.Yarat.Text = "Yarat";
+            this.Yarat.UseVisualStyleBackColor = true;
+            this.Yarat.Click += new System.EventHandler(this.hesaplama_Click);
+            // 
+            // listbox
+            // 
+            this.listbox.FormattingEnabled = true;
+            this.listbox.Location = new System.Drawing.Point(12, 12);
+            this.listbox.Margin = new System.Windows.Forms.Padding(15);
+            this.listbox.Name = "listbox";
+            this.listbox.Size = new System.Drawing.Size(281, 433);
+            this.listbox.TabIndex = 1;
+            this.listbox.SelectedIndexChanged += new System.EventHandler(this.listbox_SelectedIndexChanged);
+            // 
+            // Group
+            // 
+            this.Group.Location = new System.Drawing.Point(284, 460);
+            this.Group.Name = "Group";
+            this.Group.Size = new System.Drawing.Size(301, 45);
+            this.Group.TabIndex = 3;
+            this.Group.Text = "Grubla";
+            this.Group.UseVisualStyleBackColor = true;
+            this.Group.Click += new System.EventHandler(this.Group_Click);
+            // 
+            // groupList
+            // 
+            this.groupList.FormattingEnabled = true;
+            this.groupList.Location = new System.Drawing.Point(311, 12);
+            this.groupList.Name = "groupList";
+            this.groupList.Size = new System.Drawing.Size(766, 433);
+            this.groupList.TabIndex = 4;
+            // 
+            // showSMV
+            // 
+            this.showSMV.Location = new System.Drawing.Point(591, 460);
+            this.showSMV.Name = "showSMV";
+            this.showSMV.Size = new System.Drawing.Size(338, 45);
+            this.showSMV.TabIndex = 5;
+            this.showSMV.Text = "Show SMV";
+            this.showSMV.UseVisualStyleBackColor = true;
+            this.showSMV.Click += new System.EventHandler(this.showSMV_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1089, 517);
+            this.Controls.Add(this.showSMV);
+            this.Controls.Add(this.groupList);
+            this.Controls.Add(this.Group);
+            this.Controls.Add(this.listbox);
+            this.Controls.Add(this.Yarat);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button Yarat;
+        private System.Windows.Forms.ListBox listbox;
+        private System.Windows.Forms.Button Group;
+        private System.Windows.Forms.ListBox groupList;
+        private System.Windows.Forms.Button showSMV;
     }
 }
 
--- HyperGamy/Form1.cs
+++ HyperGamy/Form1.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using Accord.MachineLearning.DecisionTrees;
 namespace HyperGamy
 {
     public partial class Form1 : Form
@@ -16,5 +16,67 @@ namespace HyperGamy
         {
             InitializeComponent();
         }
+
+        private void listbox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        Men[] myMen = new Men[100];
+        private void hesaplama_Click(object sender, EventArgs e)
+        {
+            Random rastgele = new Random();
+            
+            for (int i=0; i < 100; i++)
+            {
+                myMen[i] = new Men();
+                myMen[i].SMV = rastgele.Next(1, 11);
+                listbox.Items.Add("Sex Count: " + myMen[i].SexCount.ToString() + " SMV: " + myMen[i].SMV.ToString());
+            }
+            
+        }
+
+        private void Group_Click(object sender, EventArgs e)
+        {
+            
+            Random rastgele = new Random();
+            for (int i = 0; i < 5000; i++)
+            {
+                int my1 = rastgele.Next(0, 100);
+                int my2 = rastgele.Next(0, 100);
+                groupList.Items.Add(myMen[my1].SMV.ToString() + "," + myMen[my2].SMV.ToString());
+                if (myMen[my1].SMV > myMen[my2].SMV)
+                {
+                    myMen[my1].SexCount++;
+                    
+                }else if(myMen[my2].SMV > myMen[my1].SMV)
+                {
+                    myMen[my2].SexCount++;
+                }
+               
+            }
+            
+        }
+
+        private void showSMV_Click(object sender, EventArgs e)
+        {
+            listbox.Items.Clear();
+            for (int i = 0; i < 100; i++)
+            {
+                listbox.Items.Add("My Man ID:" + i.ToString() + "Sex Count: " + myMen[i].SexCount.ToString() + " SMV: " + myMen[i].SMV.ToString());
+            }
+        }
+    }
+
+    public class Men
+    {
+        public int SexCount { get; set; }
+        public int SMV { get; set; }
+        public Men ()
+        {
+            SexCount = 0;
+            SMV = 0;
+        }
+        
+                
     }
 }
