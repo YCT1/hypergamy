b622603262d3c226ba5ec7472ade85f255df69ff
YCT1 <yektacan1999@gmail.com>
2018-11-16 18:29:43 +0300

    Version v2 with Release Build

--- HyperGamy/Form1.cs
+++ HyperGamy/Form1.cs
@@ -10,6 +10,7 @@ using System.Windows.Forms;
 using System.IO;
 using MathNet.Numerics;
 using MathNet.Numerics.Distributions;
+using Accord.Statistics;
 namespace HyperGamy
 {
     public partial class Form1 : Form
