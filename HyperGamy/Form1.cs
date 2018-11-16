7f73befe08b6e92eda5080fbee25b442b255133f
YCT1 <yektacan1999@gmail.com>
2018-11-16 20:21:17 +0300

    Optimization and Great Performance Increasment, Furthermore; One key mode have been added.
    Signed-off-by: YCT1 <yektacan1999@gmail.com> in Bursa

--- HyperGamy/Form1.cs
+++ HyperGamy/Form1.cs
@@ -22,11 +22,15 @@ namespace HyperGamy
         int mannumber = 100;
         private void listbox_SelectedIndexChanged(object sender, EventArgs e)
         {
-
+            Hesaplama();
         }
         Men[] myMen;
         bool gauss = false;
-        private void hesaplama_Click(object sender, EventArgs e)
+        bool writeListbox = false;        
+        
+        
+        //Hesaplama Algoritmasi
+        private void Hesaplama()
         {
             Random rastgele = new Random();
             progressBar1.Value = 0;
@@ -38,12 +42,12 @@ namespace HyperGamy
                 int negSay = 0;
                 int pozSay = 0;
                 Normal normalDist = new Normal(mean, stdDev);
-                for(int i = 0; i < myMen.Length; i++)
+                for (int i = 0; i < myMen.Length; i++)
                 {
 
                     double a = normalDist.Sample();
-                   
-                    
+
+
                     int b = Convert.ToInt32(a);
                     myMen[i] = new Men();
                     if (b > 10)
@@ -57,33 +61,38 @@ namespace HyperGamy
                         pozSay++;
                     }
                     myMen[i].SMV = b;
-                    listbox.Items.Add("Sex Count: " + myMen[i].SexCount.ToString() + " SMV: " + myMen[i].SMV.ToString());
+                    if (writeListbox)
+                    {
+                       listbox.Items.Add("Sex Count: " + myMen[i].SexCount.ToString() + " SMV: " + myMen[i].SMV.ToString());
+                    }
+
                     //Progress Bar Code
                     progressBar1.Maximum = myMen.Length;
                     progressBar1.Value++;
-                   
+
 
                 }
-                listbox.Items.Add("Poz Sayisi: " + negSay.ToString() + " Neg Sayisi: " + pozSay.ToString());
+                if (writeListbox)
+                {
+                   listbox.Items.Add("Poz Sayisi: " + negSay.ToString() + " Neg Sayisi: " + pozSay.ToString());
+                }
                 return;
             }
-            
-            
-           
-            
-            for (int i=0; i < myMen.Length; i++)
+            for (int i = 0; i < myMen.Length; i++)
             {
                 myMen[i] = new Men();
                 myMen[i].SMV = rastgele.Next(1, 11);
-                listbox.Items.Add("Sex Count: " + myMen[i].SexCount.ToString() + " SMV: " + myMen[i].SMV.ToString());
+                if (writeListbox)
+                {
+                   listbox.Items.Add("Sex Count: " + myMen[i].SexCount.ToString() + " SMV: " + myMen[i].SMV.ToString());
+                }
                 //Progress Bar Code
                 progressBar1.Maximum = myMen.Length;
                 progressBar1.Value++;
             }
-            
         }
-
-        private void Group_Click(object sender, EventArgs e)
+        //Groplama Algoritmasi
+        private void Groupla()
         {
             progressBar1.Value = 0;
             Random rastgele = new Random();
@@ -91,16 +100,17 @@ namespace HyperGamy
             {
                 int[] my = new int[100];
                 string foradd = "";
-                int[] SMV = new int[Int32.Parse(numericUpDown1.Value.ToString())+1];
+                int[] SMV = new int[Int32.Parse(numericUpDown1.Value.ToString()) + 1];
                 int maxVal = 0;
                 int index = -1;
-                for (int a=1; a < numericUpDown1.Value + 1; a++)
+                int randomGroupNumber = rastgele.Next(Int32.Parse(numericUpDown2.Value.ToString()), Int32.Parse(numericUpDown1.Value.ToString()) + 1);
+                for (int a = 1; a < randomGroupNumber + 1; a++)
                 {
-                    my[a]= rastgele.Next(0, myMen.Length);
-                    foradd = foradd +myMen[my[a]].SMV.ToString() + ",";
+                    my[a] = rastgele.Next(0, myMen.Length);
+                    foradd = foradd + myMen[my[a]].SMV.ToString() + ",";
                     SMV[a] = myMen[my[a]].SMV;
                     int thisNum = SMV[a];
-                    if(thisNum > maxVal)
+                    if (thisNum > maxVal)
                     {
                         maxVal = thisNum;
                         index = a;
@@ -108,28 +118,33 @@ namespace HyperGamy
 
                 }
                 myMen[my[index]].SexCount++;
-                groupList.Items.Add(foradd);
+                if (writeListbox)
+                {
+                    groupList.Items.Add(foradd);
+                }
                 //Progress Bar Code
                 progressBar1.Maximum = myMen.Length;
                 progressBar1.Value++;
             }
-
         }
-
-        private void showSMV_Click(object sender, EventArgs e)
+        //Sex Yapma Algoritmasi
+        private void showSMVf()
         {
             progressBar1.Value = 0;
             listbox.Items.Clear();
             for (int i = 0; i < myMen.Length; i++)
             {
-                listbox.Items.Add("ID: " + i.ToString() + " Sex Count: " + myMen[i].SexCount.ToString() + " SMV: " + myMen[i].SMV.ToString());
+                if (writeListbox)
+                {
+                   listbox.Items.Add("ID: " + i.ToString() + " Sex Count: " + myMen[i].SexCount.ToString() + " SMV: " + myMen[i].SMV.ToString());
+                }
                 //Progress Bar Code
                 progressBar1.Maximum = myMen.Length;
                 progressBar1.Value++;
             }
         }
-
-        private void topla_Click(object sender, EventArgs e)
+        //Topla Algoritmasi
+        private void toplaf()
         {
             progressBar1.Value = 0;
             groupList.Items.Clear();
@@ -139,35 +154,35 @@ namespace HyperGamy
             int toplamSex = 0;
             for (int i = 0; i < myMen.Length; i++)
             {
-                toplam[myMen[i].SMV]= toplam[myMen[i].SMV] + myMen[i].SexCount;
+                toplam[myMen[i].SMV] = toplam[myMen[i].SMV] + myMen[i].SexCount;
                 toplamPeople[myMen[i].SMV]++;
                 toplamSex = toplamSex + myMen[i].SexCount;
                 //////////////Progress Bar Code
-                progressBar1.Maximum = myMen.Length+toplamPeople.Length+toplam.Length;
+                progressBar1.Maximum = myMen.Length + toplamPeople.Length + toplam.Length;
                 progressBar1.Value++;
             }
-            for(int i = 1; i < toplamPeople.Length; i++)
+            for (int i = 1; i < toplamPeople.Length; i++)
             {
                 groupList.Items.Add(toplamPeople[i]);
-                
+
                 //////////////Progress Bar Code
                 progressBar1.Value++;
             }
             groupList.Items.Add("AAAAAAAAAAAAAAA");
             for (int i = 0; i < toplam.Length; i++)
             {
-                groupList.Items.Add(i.ToString() + "," + toplam[i]);
+                groupList.Items.Add(i.ToString() + ";" + toplam[i]);
                 //////////////Progress Bar Code
                 progressBar1.Value++;
             }
             groupList.Items.Add("AAAAAAAAAAA");
             groupList.Items.Add(toplamSex);
             groupList.Items.Add("AAAAAAAAAAA");
-            for(int i = 1; i < toplam.Length; i++)
+            for (int i = 1; i < toplam.Length; i++)
             {
                 float a = toplam[i];
                 float b = toplamPeople[i];
-               avrSex[i] = a / b;
+                avrSex[i] = a / b;
                 groupList.Items.Add(avrSex[i]);
             }
             if (csvfile.Checked)
@@ -180,6 +195,31 @@ namespace HyperGamy
                 }
                 myOutputStream.Close();
             }
+        }
+
+        private void hesaplama_Click(object sender, EventArgs e)
+        {
+            writeListbox = true;
+            Hesaplama();
+        }
+
+        private void Group_Click(object sender, EventArgs e)
+        {
+            writeListbox = true;
+            Groupla();
+
+        }
+
+        private void showSMV_Click(object sender, EventArgs e)
+        {
+            writeListbox = true;
+            showSMVf();
+        }
+
+        private void topla_Click(object sender, EventArgs e)
+        {
+            writeListbox = true;
+            toplaf();
         }
 
         private void numMen_ValueChanged(object sender, EventArgs e)
@@ -227,16 +267,50 @@ namespace HyperGamy
             }
             myOutputStream.Close();
         }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            if (lockofnumericvalues.Checked)
+            {
+                numericUpDown2.Value = numericUpDown1.Value;
+            }
+            else { return; }
+        }
+
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+            if (lockofnumericvalues.Checked)
+            {
+                numericUpDown1.Value = numericUpDown2.Value;
+            }
+            else { return; }
+        }
+
+        private void doAll_Click(object sender, EventArgs e)
+        {
+            writeListbox = false;
+            Hesaplama();
+            Groupla();
+            //showSMVf();
+            toplaf();
+        }
     }
 
     public class Men
     {
         public int SexCount { get; set; }
         public int SMV { get; set; }
+        public int L { get; set; }
+        public int M { get; set; }
+        public int S { get; set; }
         public Men ()
         {
             SexCount = 0;
             SMV = 0;
+            L = 0;
+            M = 0;
+            S = 0;
+            
         }
         
                 
